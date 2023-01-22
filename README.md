f442003d2f3614e7035f390e9d329d454d2da84e
Haritz <harits.muhammad.only@gmail.com>
2023-01-22 21:53:43 +0700

    configuring project setting runserver done, configuring app settings done, setup selenium in django

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # meetdrf
 Place to learn Django Rest Framework
+
+Step by Step
+A. Django Preparation
+1. Install django framework package (in terminal)
+2. Start django project (in terminal django-admin startproject project_name.)
+3. Setup django project
+   a. create_app(in terminal manage.py startapp app_name)
+   b. install app (in project_name/settings.py INSTALLED APPS) into project
+   c. create html file tamplate (in app_name/template_name.html)
+   d. setup template DIRS path (in project_name/settings.py TEMPLATES)
+   e. add static path (in project_name/settings.py STATIC_URL), static files(css, js, etc.)
+   f. add media path (in project_name/settings.py MEDIA_URL), media files
+4. Setup django app
+   a. create urls.py (in app_name/)
+   b. include desired urls (app_name/urls) for the app (in project_name/urls.py urlpattern)
+   c. create views function or classes (in app_name/views.py)
+   d. open route from url to views function (in app_name/urls.py)
+
+B. Web Scraper Preparation
+1. Install Selenium (in terminal)
+2. Create 
--- dsapp/views.py
+++ dsapp/views.py
@@ -1,5 +1,53 @@
-from django.shortcuts import render
+from dataclasses import dataclass
 
-# Create your views here.
+from django.shortcuts import render
+from dataclasses import dataclass
+
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+# The function defined is called a ds. When this function is called, it will render an HTML file called `app_name.html`)
 def ds(request):
     return render(request, 'ds.html', {})
+
+# The function defined is called a **view function**. When this function is called,
+# it will render an HTML file called `app_name.html`.
+@dataclass
+class DjangoScraper:
+    def webdriver_setup(self):
+        ff_opt = Options()
+        ff_opt.add_argument("--windows-size=1920,1080",
+                            "--disable-gpu",
+                            "--disable-extensions",
+                            "--no-sandbox",
+                            "--incognito",
+                            # "--headless",
+                            )
+        driver = WebDriver(options=ff_opt)
+        return driver
+
+    def auto_login(self, driver, url, username, password):
+        driver.get(url)
+        password_input = driver.find_element_by_xpath("//input[@type='password']")
+        password_input.send_keys(password)
+        username_input = password_input.find_element_by_xpath(
+            ".//preceding::input[not(@type='hidden')]")
+        username_input.send_keys(username)
+        form_element = password_input.find_element_by_xpath(".//ancestor::form")
+        submit_button = form_element.find_element_by_xpath(
+            ".//*[@type='submit']").click()
+        return driver
+
+    def scrap(request):
+        options = Options()
+        options.headless = True
+        options.add_argument("--window-size=1920,1080")
+
+        driver = webdriver.Chrome(options=options)
+
+        USERNAME = os.getenv("USERNAME")
+        PASSWORD = os.getenv("PASSWORD")
+
+        autologin(driver, 'https://maya.um.edu.my/sitsvision/wrd/siw_lgn',
+                  USERNAME, PASSWORD)
